3d23f18575715005c2e7ad3121d38c4a5a1bafb2
kurachyan <e_tamakou@outlook.jp>
2015-03-22 19:46:51 +0900

    Ｃｈｅｃｋ評価追加 補助機能追加　及び、動作確認

---
+++ MyStack2/CommonMethods.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+namespace MyStack2
+{
+
+    public sealed partial class MainPage : Page
+    {
+        //出力用テキストボックス
+        TextBox _TextBoxResult = null;
+        public TextBox TextBoxResult {
+            get{
+                if (_TextBoxResult == null)
+                {
+                    var tb = FindFirstElement<TextBox>(this);
+                    TextBoxResult = tb;
+                }
+                return _TextBoxResult;
+            }
+            set { 
+                _TextBoxResult = value;
+                ScrollViewer.SetVerticalScrollBarVisibility(_TextBoxResult, ScrollBarVisibility.Visible);
+                _TextBoxResult.AcceptsReturn = true;
+                _TextBoxResult.TextWrapping = TextWrapping.Wrap;
+                ScrollViewerOfTextBoxResult = FindFirstElement<ScrollViewer>(TextBoxResult);
+            } 
+        }
+        //出力用テキストボックスのスクロールビュワー
+        ScrollViewer _sv = null;       
+        public ScrollViewer ScrollViewerOfTextBoxResult{
+            get{
+                if (_sv == null)
+                {
+                    _sv =  FindFirstElement<ScrollViewer>(TextBoxResult);
+                }
+                return _sv;
+            }
+            set{ _sv = value;}
+        }
+
+
+        //テキストボックスクリア
+        void ClearResultTextBox()
+        {
+            TextBoxResult.Text = "";
+        }
+        //結果を表示するテキストボックスに出力
+        void WriteResult(string format, params object[] args)
+        {
+            TextBoxResult.Text += string.Format(format, args);
+        }
+        //結果を表示するテキストボックスに改行を出力
+        void WriteLineResult()
+        {
+            TextBoxResult.Text += Environment.NewLine;
+            ScrollViewerOfTextBoxResult.ChangeView(null, ScrollViewerOfTextBoxResult.ScrollableHeight, null);
+        }
+        //結果を表示するテキストボックスに出力（改行付）
+        void WriteLineResult(string format, params object[] args)
+        {
+            WriteResult(format, args);
+            WriteLineResult();
+            //出力ウィンドウに出力
+            Debug.WriteLine(format, args);
+        }
+        //指定した型を持つ子要素を取得する
+        private static T FindFirstElement<T>(DependencyObject parent) where T : class
+        {
+            var childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            T element = default(T);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var childObj = VisualTreeHelper.GetChild(parent, i);
+                element = childObj as T;
+                if (element == null)
+                {
+                    element = FindFirstElement<T>(childObj);
+                }
+                if (element != null)
+                {
+                    break;
+                }
+            }
+            return element;
+        }
+
+
+    }
+}
--- MyStack2/MainPage.xaml.cs
+++ MyStack2/MainPage.xaml.cs
@@ -22,9 +22,107 @@ namespace MyStack2
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        #region コンストラクタ
         public MainPage()
-        {
+        {   // アプリケーション　コンストラクタ
             this.InitializeComponent();
+
+            // 初期表示をクリアする
+            ClearResultTextBox();
+//			TextBox02.Text = "";
         }
+        #endregion
+
+        #region ［Ｐｕｓｈ（Ｎｕｍｂｅｒ）］ボタン押下
+        private void Button01_Click(object sender, RoutedEventArgs e)
+        {   // [Push(Num)]ボタン押下
+            WriteLineResult(@"[Push (Number)]");
+
+        }
+        #endregion
+
+        #region ［Ｐｏｐ（Ｎｕｍｂｅｒ）］ボタン押下
+        private void Button02_Click(object sender, RoutedEventArgs e)
+        {   // [Pop(Num)]ボタン押下
+            WriteLineResult(@"[Pop (Number)]");
+
+        }
+        #endregion
+
+        #region ［Ｑｕｅ（Ｎｕｍｂｅｒ）］ボタン押下
+        private void Button03_Click(object sender, RoutedEventArgs e)
+        {   // [Que(Num)]ボタン押下
+            WriteLineResult(@"[Que (Number)]");
+
+        }
+        #endregion
+
+        #region ［Ｃｌｅａｒ（Ｎｕｍｂｅｒ）］ボタン押下
+        private void Button04_Click(object sender, RoutedEventArgs e)
+        {   // [Clear(Num)]ボタン押下
+            WriteLineResult(@"[Clear (Number)]");
+
+        }
+        #endregion
+
+        #region ［Ｃｈｅｃｋ（Ｎｕｍｂｅｒ）］ボタン押下
+        private void Button05_Click(object sender, RoutedEventArgs e)
+        {   // [Check(Num)]ボタン押下
+            WriteLineResult(@"[Check (Number)]");
+
+        }
+        #endregion
+
+        #region ［Ｐｕｓｈ（Ｓｔｒｉｎｇ）］ボタン押下
+        private void Button11_Click(object sender, RoutedEventArgs e)
+        {   // [Push(Str)]ボタン押下
+            WriteLineResult(@"[Push (String)]");
+
+        }
+        #endregion
+
+        #region ［Ｐｏｐ（Ｓｔｒｉｎｇ）］ボタン押下
+        private void Button12_Click(object sender, RoutedEventArgs e)
+        {   // [Pop(Str)]ボタン押下
+            WriteLineResult(@"[Pop (String)]");
+
+        }
+        #endregion
+
+        #region ［Ｑｕｅ（Ｓｔｒｉｎｇ）］ボタン押下
+        private void Button13_Click(object sender, RoutedEventArgs e)
+        {   // [Que(Str)]ボタン押下
+            WriteLineResult(@"[Que (String)]");
+
+        }
+        #endregion
+
+        #region ［Ｃｌｅａｒ（Ｓｔｒｉｎｇ）］ボタン押下
+        private void Button14_Click(object sender, RoutedEventArgs e)
+        {   // [Clear(Str)]ボタン押下
+            WriteLineResult(@"[Clear (String)]");
+
+        }
+        #endregion
+
+        #region ［Ｃｈｅｃｋ（Ｓｔｒｉｎｇ）］ボタン押下
+        private void Button15_Click(object sender, RoutedEventArgs e)
+        {   // [Check(Str)]ボタン押下
+            WriteLineResult(@"[Check (String)]");
+
+        }
+        #endregion
+
+        #region ［Ｒｅｓｅｔ］ボタン押下
+        private void Button20_Click(object sender, RoutedEventArgs e)
+        {   // [Reset]ボタン押下
+            ClearResultTextBox();
+
+//            TextBox02.Text = "";
+
+        }
+        #endregion
+
+
     }
 }
